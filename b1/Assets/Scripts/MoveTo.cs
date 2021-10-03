01f467f1d003c5b8dd7a83d8407985f7e47e6d66
KangxiXiao <xiaokangxi123@gmail.com>
2021-10-02 23:14:20 -0400

    agent pathfinding

---
+++ b1/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+    
+public class MoveTo : MonoBehaviour {
+    private NavMeshAgent agent;
+    public LayerMask canBeClickedOn;
+    
+    
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+        
+    void Update() {
+         if (Input.GetMouseButtonDown(1)) {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+                
+        if (Physics.Raycast(myRay, out hit, 100, canBeClickedOn)) {
+            agent.SetDestination(hit.point);
+            agent.velocity = Vector3.zero;
+            }
+        }
+        
+    }
+}
